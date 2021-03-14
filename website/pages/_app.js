98a6f6a2aa8cb9890b41b73cb4cf17aa883ed549
krehwell <fahrimuham001@gmail.com>
2021-03-14 10:32:20 +0800

    WS-FE: merge all component pages and combined all styles. dump everything to index

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,3 +1,10 @@
+/// STYLES
+import "../styles/layout.css";
+import "../styles/components/header.css";
+import "../styles/components/footer.css";
+import "../styles/components/alternateHeader.css";
+import "../styles/pages/_error.css";
+
 export default function MyApp({Component, pageProps}) {
     return <Component {...pageProps} />;
 }
--- website/pages/index.js
+++ website/pages/index.js
@@ -1,9 +1,22 @@
 import { Component } from "react"
 
+/// COMPONENTS
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import AlternateHeader from "../components/alternateHeader";
+
 export default class Home extends Component {
     render () {
         return (
-          <div>Your Next.js App</div>
+          <div className="layout-wrapper">
+            <HeadMetadata title="Coder News" description="News and discussion for software engineers"/>
+            <Header />
+            <div className="homepage-content-container">
+              Your Next.js App
+            </div>
+            <Footer />
+          </div>
         )
     }
 }
