6c1dd87471e3c472209cd8c7b8ce2ea974045578
krehwell <fahrimuham001@gmail.com>
2021-03-14 10:28:23 +0800

    WS-FE: make an error redirection page

---
+++ website/pages/404.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+import HeadMetadata from "../components/headMetadata.js";
+
+export default class _404 extends Component {
+    render() {
+        return (
+          <div className="error-wrapper">
+            <HeadMetadata title="Unkown | Coder News" />
+            <span>An error occurred. (ERROR: 400)</span>
+          </div>
+        )
+    }
+}
---
+++ website/pages/_error.js
@@ -0,0 +1,14 @@
+import { Component } from "react"
+
+import HeadMetadata from "../components/headMetadata.js"
+
+export default class extends Component {
+    render () {
+        return (
+            <div className="error-wrapper">
+                <HeadMetadata title="Error | Coder News" />
+                <span>An error occurred. (ERROR: 500)</span>
+            </div>
+        )
+    }
+}
