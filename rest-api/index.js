f8d2eb6735d26df805752bca2ab35533e2e0dfeb
krehwell <fahrimuham001@gmail.com>
2021-03-19 14:22:31 +0800

    WS-BE: connect rest-api with mongodb

--- rest-api/index.js
+++ rest-api/index.js
@@ -3,13 +3,29 @@ const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 /// REST-API CONFIG
 const PORT = process.env.PORT || 5000
 
 const app = express();
 
+
 /// DB CONNECTION
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@heckar-news.d3u83.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoString, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+mongoose.connection.on("error", function(error) {
+    if (process.env.NODE_ENV === "development") {
+        console.log(error)
+    }
+});
+
+mongoose.connection.on("open", function() {
+    console.log("Connected to MongoDB database.")
+});
 
 
 /// REST-API CONFIG
