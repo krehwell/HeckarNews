5f61075e16752a3cb1950bfa7b32ca0e48b47df5
krehwell <fahrimuham001@gmail.com>
2021-03-22 21:22:23 +0800

    WS: add login page frontend, barebone

---
+++ website/pages/login.js
@@ -0,0 +1,195 @@
+import { Component } from "react";
+
+import HeadMetadata from "../components/headMetadata.js";
+
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+
+            // login
+            loginUsernameInputValue: "",
+            loginPasswordInputValue: "",
+            loginCredentialError: false,
+            loginSubmitError: false,
+
+            //create account
+            createAccountUsernameInputValue: "",
+            createAcountPasswordInputValue: "",
+            createAccountUsernameExistsError: false,
+            createAccountUsernameLengthError: false,
+            createAccountPasswordLengthError: false,
+            createAccountSubmitError: false,
+        };
+    }
+
+    updateLoginUsernameInputValue = (event) => {
+        this.setState({ loginUsernameInputValue: event.target.value });
+    };
+
+    updateLoginPasswordInputValue = (event) => {
+        this.setState({ loginPasswordInputValue: event.target.value });
+    };
+
+    updateCreateAccountUsernameInputValue = (event) => {
+        this.setState({ createAccountUsernameInputValue: event.target.value });
+    };
+
+    updateCreateAccountPasswordInputValue = (event) => {
+        this.setState({ createAcountPasswordInputValue: event.target.value });
+    };
+
+    submitLogin = () => {
+    };
+
+    submitCreateAccount = () => {
+        if (this.state.loading) return;
+
+        const username = this.state.createAccountUsernameInputValue;
+        const password = this.state.createAcountPasswordInputValue;
+
+        if (username.length < 2 || username.length > 15) {
+            this.setState({
+                createAccountUsernameExistsError: false,
+                createAccountUsernameLengthError: true,
+                createAccountPasswordLengthError: false,
+                createAccountSubmitError: false,
+            });
+        } else if (password.length < 8) {
+            this.setState({
+                createAccountUsernameExistsError: false,
+                createAccountUsernameLengthError: false,
+                createAccountPasswordLengthError: true,
+                createAccountSubmitError: false,
+            });
+        } else {
+            this.setState({ loading: true });
+
+            const self = this;
+
+            // make request to the REST API here
+        }
+    };
+
+    render() {
+        return (
+            <div className="login-wrapper">
+                <HeadMetadata title="Login | Coder News" />
+
+                {/*LOGIN SECTION*/}
+                {this.state.loginCredentialError ? (
+                    <div className="login-error-msg">
+                        <span>Bad login.</span>
+                    </div>
+                ) : null}
+                {this.state.loginSubmitError ? (
+                    <div className="login-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                ) : null}
+                <div className="login-header">
+                    <span>Login</span>
+                </div>
+                <div className="login-input-item">
+                    <div className="login-input-item-label">
+                        <span>username:</span>
+                    </div>
+                    <div className="login-input-item-input">
+                        <input
+                            type="text"
+                            value={this.state.loginUsernameInputValue}
+                            onChange={this.updateLoginUsernameInputValue}
+                        />
+                    </div>
+                </div>
+                <div className="login-input-item">
+                    <div className="login-input-item-label">
+                        <span>password:</span>
+                    </div>
+                    <div className="login-input-item-input">
+                        <input
+                            type="password"
+                            value={this.state.loginPasswordInputValue}
+                            onChange={this.updateLoginPasswordInputValue}
+                        />
+                    </div>
+                </div>
+                <div className="login-submit-btn">
+                    <input
+                        type="submit"
+                        value="login"
+                        onClick={() => this.submitLogin()}
+                    />
+                </div>
+                <div className="login-input-item-forgot-text">
+                    <span>
+                        <a href="/forgot">Forgot your Password?</a>
+                    </span>
+                </div>
+
+                {/*CREATE ACCOUNT SECTION*/}
+                {this.state.createAccountUsernameExistsError ? (
+                    <div className="login-error-msg">
+                        <span>That username is taken.</span>
+                    </div>
+                ) : null}
+                {this.state.createAccountUsernameLengthError ? (
+                    <div className="login-error-msg">
+                        <span>
+                            Username must be between 2 and 15 characters long.
+                        </span>
+                    </div>
+                ) : null}
+                {this.state.createAccountPasswordLengthError ? (
+                    <div className="login-error-msg">
+                        <span>Passwords should be at least 8 characters.</span>
+                    </div>
+                ) : null}
+                {this.state.createAccountSubmitError ? (
+                    <div className="login-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                ) : null}
+                <div className="login-header">
+                    <span>Create Account</span>
+                </div>
+                <div className="login-input-item">
+                    <div className="login-input-item-label">
+                        <span>username:</span>
+                    </div>
+                    <div className="login-input-item-input">
+                        <input
+                            type="text"
+                            value={this.state.createAccountUsernameInputValue}
+                            onChange={
+                                this.updateCreateAccountUsernameInputValue
+                            }
+                        />
+                    </div>
+                </div>
+                <div className="login-input-item">
+                    <div className="login-input-item-label">
+                        <span>password:</span>
+                    </div>
+                    <div className="login-input-item-input">
+                        <input
+                            type="password"
+                            value={this.state.createAcountPasswordInputValue}
+                            onChange={
+                                this.updateCreateAccountPasswordInputValue
+                            }
+                        />
+                    </div>
+                </div>
+                <div className="login-submit-btn">
+                    <input
+                        type="submit"
+                        value="create account"
+                        onClick={() => this.submitCreateAccount()}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
