f194cd17faf59b0057fb50fb2f8cdb9591448e44
krehwell <fahrimuham001@gmail.com>
2021-03-24 23:45:21 +0800

    WS-BE: made a user model

---
+++ rest-api/models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    authToken: String,
+    authTokenExpiration: Number,
+    resetPassword: String,
+    resetPasswordExpiration: Number,
+
+    email: {
+        type: String,
+        lowercase: true,
+        default: "",
+    },
+
+    created: Number,
+
+    karma: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+
+    about: {
+        type: String,
+        default: "",
+    },
+
+    showDead: {
+        type: Boolean,
+        default: false,
+    },
+
+});
+
+UserSchema.pre("save", function (next) {
+    const user = this;
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+UserSchema.methods.comparePassword = (pw, cb) => {
+    bcrypt.compare(pw, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        }
+
+        cb(null, isMatch);
+    });
+};
+
+UserSchema.index({ username: 1 }, { unique: true });
+
+module.exports = mongoose.model("User", UserSchema);
