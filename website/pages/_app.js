0623b5ff98aafcd7d1440ffc5408aaabbc9f29e0
krehwell <fahrimuham001@gmail.com>
2021-05-07 11:26:25 +0800

    WS-FE: add reset password page

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -6,6 +6,7 @@ import "../styles/components/alternateHeader.css";
 import "../styles/pages/_error.css";
 import "../styles/pages/login.css";
 import "../styles/pages/forgot.css";
+import "../styles/pages/reset.css";
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
---
+++ website/pages/reset.js
@@ -0,0 +1,102 @@
+import { Component } from "react";
+
+import HeadMetadata from "../components/headMetadata.js";
+import AlternateHeader from "../components/alternateHeader.js";
+
+export default class extends Component {
+    constructor() {
+        super();
+        this.state = {
+            passwordInputValue: "",
+            passwordLengthError: false,
+            expiredTokenError: false,
+            invalidTokenError: false,
+            submitError: false,
+        };
+    }
+
+    updatePasswordInputValue = (event) => {
+        this.setState({ passwordInputValue: event.target.value });
+    };
+
+    submitRequest = () => {
+        if (this.state.loading) return;
+
+        if (this.state.passwordInputValue.length < 8) {
+            this.setState({
+                passwordLengthError: true,
+                expiredTokenError: false,
+                invalidTokenError: false,
+                submitError: false,
+            });
+        } else {
+            this.setState({ loading: true });
+
+            const self = this;
+
+            // request to REST API goes here
+        }
+    };
+
+    render() {
+        return (
+            <div className="layout-wrapper">
+                <HeadMetadata title="Reset Password | Coder News" />
+                <AlternateHeader displayMessage="Reset Password" />
+                <div className="reset-password-content-container">
+                    {this.state.passwordLengthError ? (
+                        <div className="reset-password-error-msg">
+                            <span>
+                                Passwords should be at least 8 characters.
+                            </span>
+                        </div>
+                    ) : null}
+                    {this.state.expiredTokenError ? (
+                        <div className="reset-password-error-msg">
+                            <span>Reset token has expired.</span>
+                        </div>
+                    ) : null}
+                    {this.state.invalidTokenError ? (
+                        <div className="reset-password-error-msg">
+                            <span>Reset token is invalid.</span>
+                        </div>
+                    ) : null}
+                    {this.state.submitError ? (
+                        <div className="reset-password-error-msg">
+                            <span>An error occurred.</span>
+                        </div>
+                    ) : null}
+                    <div className="reset-password-input-item">
+                        <div className="reset-password-input-item-label">
+                            <span>New Password:</span>
+                        </div>
+                        <div className="reset-password-input-item-input">
+                            <input
+                                type="password"
+                                value={this.state.passwordInputValue}
+                                onChange={this.updatePasswordInputValue}
+                            />
+                        </div>
+                    </div>
+                    <div className="reset-password-submit-btn">
+                        <input
+                            type="submit"
+                            value="Change"
+                            onClick={() => this.submitRequest()}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export async function getServerSideProps({ query }) {
+    console.log(query);
+    return {
+        props: {
+            resetToken: query.token,
+            username: query.username,
+        },
+    };
+}
