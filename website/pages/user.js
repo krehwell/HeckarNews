78dcd18d18ddbc6f3d98d0dcf334b9343f4cc7a9
krehwell <fahrimuham001@gmail.com>
2021-05-12 15:53:35 +0800

    WS-FE: fix bug on update user input value

--- website/pages/user.js
+++ website/pages/user.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import moment from "moment";
 
 import Header from "../components/header.js";
@@ -26,7 +26,7 @@ export default function User({
     });
 
     const updateAboutInputValue = (event) => {
-        setState({ aboutInputValue: event.target.value });
+        setState({ ...state, aboutInputValue: event.target.value });
     };
 
     const setInitialTextareaHeight = () => {
