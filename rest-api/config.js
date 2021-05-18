34a2958904d67325b220f666529b5d740488aad0
krehwell <fahrimuham001@gmail.com>
2021-05-18 18:55:34 +0800

    WS-BE: update config to have item to be shown max days

--- rest-api/config.js
+++ rest-api/config.js
@@ -1,8 +1,10 @@
 const config = {
-    productionWebsiteUrl: "https://heckarnews.com",
+    productionWebsiteUrl: "https://news.krehwell.com",
     userCookieExpirationLengthInDays: 365,
     hrsUntilUnvoteExpires: 1,
     hrsUntilEditAndDeleteExpires: 2,
+    maxAgeOfRankedItemsInDays: 10,
+    itemsPerPage: 30,
 };
 
 module.exports = config;
