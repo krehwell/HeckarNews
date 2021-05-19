ed93f39c87098977b2ce89230ef552cc2bceb458
krehwell <fahrimuham001@gmail.com>
2021-05-19 19:27:58 +0800

    WS-FE: add news page

---
+++ website/pages/news.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import HeadMetadata from "../components/headMetadata.js";
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import ItemsList from "../components/itemsList.js";
+
+import getRankedItemsByPage from "../api/items/getRankedItemsByPage.js";
+
+export default function News({
+    items,
+    authUserData,
+    page,
+    isMore,
+    getDataError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata title="Coder News" />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+            />
+            <div className="items-list-content-container">
+                {!getDataError ? (
+                    <>
+                        <ItemsList
+                            items={items}
+                            goToString={goToString}
+                            userSignedIn={authUserData.userSignedIn}
+                            currUsername={authUserData.username}
+                            showHideOption={true}
+                            showRank={true}
+                            isMoreLink={`/news?page=${page + 1}`}
+                            isMore={isMore}
+                        />
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const apiResult = await getRankedItemsByPage(page, req);
+    // console.log(apiResult);
+
+    return {
+        props: {
+            items: (apiResult && apiResult.items) || {},
+            authUserData:
+                apiResult && apiResult.authUser ? apiResult.authUser : {},
+            page: page || 0,
+            isMore: (apiResult && apiResult.isMore) || false,
+            getDataError: (apiResult && apiResult.getDataError) || false,
+            goToString: page > 1 ? `news?page=${page}` : "news",
+        },
+    };
+}
