3a79f75f8492da301a17743a881278537b116a04
krehwell <fahrimuham001@gmail.com>
2021-05-21 02:07:23 +0800

    WS-FE: add from page

---
+++ website/pages/from.js
@@ -0,0 +1,75 @@
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import ItemsList from "../components/itemsList.js";
+
+import getItemsBySiteDomain from "../api/items/getItemsBySiteDomain.js";
+
+export default function From({
+    items,
+    authUserData,
+    site,
+    page,
+    isMore,
+    getDataError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata title={`Submissions from ${site} | HeckarNews`} />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+                label="from"
+            />
+            <div className="items-list-content-container">
+                {!getDataError ? (
+                    <>
+                        <ItemsList
+                            items={items}
+                            goToString={goToString}
+                            userSignedIn={authUserData.userSignedIn}
+                            currUsername={authUserData.username}
+                            showWebLink={true}
+                            showPastLink={true}
+                            isMoreLink={`/from?site=${site}&page=${
+                                page + 1
+                            }`}
+                            isMore={isMore}
+                        />
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const site = query.site ? query.site : "";
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const apiResult = await getItemsBySiteDomain(site, page, req);
+
+    return {
+        props: {
+            items: (apiResult && apiResult.items) || {},
+            authUserData:
+                apiResult && apiResult.authUser ? apiResult.authUser : {},
+            site: site || "",
+            page: page || 0,
+            isMore: (apiResult && apiResult.isMore) || false,
+            getDataError: (apiResult && apiResult.getDataError) || false,
+            goToString:
+                page > 1
+                    ? `from?site=${site}&page=${page}`
+                    : `from?site=${site}`,
+        },
+    };
+}
