ff930da05ce952db33190349570a5d85a16478e9
krehwell <fahrimuham001@gmail.com>
2021-05-21 00:12:27 +0800

    WS-FE: add show new page

---
+++ website/pages/shownew.js
@@ -0,0 +1,70 @@
+import { Component } from "react";
+
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import ItemsList from "../components/itemsList.js";
+
+import getNewestShowItemsByPage from "../api/items/getNewestShowItemsByPage.js";
+
+export default function ShowNew({
+    items,
+    authUserData,
+    page,
+    isMore,
+    getDataError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata title="New Show | HeckarNews" />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+                label="shownew"
+            />
+            <div className="items-list-content-container">
+                {!getDataError ? (
+                    <>
+                        <ItemsList
+                            items={items}
+                            goToString={goToString}
+                            userSignedIn={authUserData.userSignedIn}
+                            currUsername={authUserData.username}
+                            showHideOption={true}
+                            showRank={true}
+                            isMoreLink={`/shownew?page=${page + 1}`}
+                            isMore={isMore}
+                        />
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const apiResult = await getNewestShowItemsByPage(page, req);
+    // console.log(apiResult);
+
+    return {
+        props: {
+            items: (apiResult && apiResult.items) || {},
+            authUserData:
+                apiResult && apiResult.authUser ? apiResult.authUser : {},
+            page: page || 0,
+            isMore: (apiResult && apiResult.isMore) || false,
+            getDataError: (apiResult && apiResult.getDataError) || false,
+            goToString: page > 1 ? `news?page=${page}` : "shownew",
+        },
+    };
+}
