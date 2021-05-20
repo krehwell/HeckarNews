26f82317174d090be9e83901e55bee2010b71809
krehwell <fahrimuham001@gmail.com>
2021-05-20 23:43:44 +0800

    WS-BE: add get newest show api

--- rest-api/routes/items/api.js
+++ rest-api/routes/items/api.js
@@ -871,4 +871,121 @@ module.exports = {
             };
         }
     },
+
+    getNewestShowItemsByPage: async (page, authUser) => {
+        if (!authUser.userSignedIn) {
+            // GET NEWEST ITEM FOR A NON-SIGNED-IN USER
+            const [items, totalItemCount] = await Promise.all([
+                ItemModel.find({ type: "show", dead: false })
+                    .sort({ _id: -1 })
+                    .skip((page - 1) * config.itemsPerPage)
+                    .limit(config.itemsPerPage)
+                    .lean(),
+                ItemModel.countDocuments({ type: "show", dead: false }).lean(),
+            ]);
+
+            for (i = 0; i < items.length; i++) {
+                items[i].rank = (page - 1) * config.itemsPerPage + (i + 1);
+            }
+
+            return {
+                success: true,
+                items: items,
+                isMore:
+                    totalItemCount >
+                    (page - 1) * config.itemsPerPage + config.itemsPerPage
+                        ? true
+                        : false,
+            };
+        } else {
+            // GET NEWEST ITEM FOR A SIGNED-IN USER
+            const hiddenDocs = await UserHiddenModel.find({
+                username: authUser.username,
+            })
+                .lean()
+                .exec();
+
+            // collect each item id to retrieve user upvote next
+            let arrayOfHiddenItems = [];
+
+            for (let i = 0; i < hiddenDocs.length; i++) {
+                arrayOfHiddenItems.push(hiddenDocs[i].id);
+            }
+
+            // query to get the item with type show
+            let itemsDbQuery = {
+                type: "show",
+                id: {
+                    $nin: arrayOfHiddenItems,
+                },
+            };
+
+            if (!authUser.showDead) itemsDbQuery.dead = false;
+
+            const items = await ItemModel.find(itemsDbQuery)
+                .sort({ _id: -1 })
+                .skip((page - 1) * config.itemsPerPage)
+                .limit(config.itemsPerPage)
+                .lean()
+                .exec();
+
+            // collect each item id to retrieve user upvote next
+            let arrayOfItemIds = [];
+
+            for (let i = 0; i < items.length; i++) {
+                arrayOfItemIds.push(items[i].id);
+            }
+
+            const [userItemVoteDocs, totalItemCount] = await Promise.all([
+                UserVoteModel.find({
+                    username: authUser.username,
+                    id: { $in: arrayOfItemIds },
+                    type: "item",
+                }).lean(),
+                ItemModel.countDocuments(itemsDbQuery).lean(),
+            ]);
+
+            for (let i = 0; i < items.length; i++) {
+                // assign rank to each item
+                items[i].rank = (page - 1) * config.itemsPerPage + (i + 1);
+
+                // is item allowed to be edited or deleted?
+                for (let i = 0; i < items.length; i++) {
+                    items[i].rank = (page - 1) * config.itemsPerPage + (i + 1);
+
+                    if (items[i].by === authUser.username) {
+                        const hasEditAndDeleteExpired =
+                            items[i].created +
+                                3600 * config.hrsUntilEditAndDeleteExpires <
+                                moment().unix() || items[i].commentCount > 0;
+
+                        items[i].editAndDeleteExpired = hasEditAndDeleteExpired;
+                    }
+                }
+
+                const voteDoc = userItemVoteDocs.find((voteDoc) => {
+                    return voteDoc.id === items[i].id;
+                });
+
+                if (voteDoc) {
+                    items[i].votedOnByUser = true;
+                    items[i].unvoteExpired =
+                        voteDoc.date + 3600 * config.hrsUntilUnvoteExpires <
+                        moment().unix()
+                            ? true
+                            : false;
+                }
+            }
+
+            return {
+                success: true,
+                items: items,
+                isMore:
+                    totalItemCount >
+                    (page - 1) * config.itemsPerPage + config.itemsPerPage
+                        ? true
+                        : false,
+            };
+        }
+    },
 };
