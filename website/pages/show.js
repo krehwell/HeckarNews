8c5a4642afa04fb7b73c5fa436057d55290f8019
krehwell <fahrimuham001@gmail.com>
2021-05-20 23:02:07 +0800

    WS-FE: add show page

---
+++ website/pages/show.js
@@ -0,0 +1,76 @@
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import ItemsList from "../components/itemsList.js";
+
+import getRankedShowItemsByPage from "../api/items/getRankedShowItemsByPage.js";
+
+export default function Show({
+    items,
+    authUserData,
+    page,
+    isMore,
+    getDataError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata title="Show | HeckarNews" />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+                pageName="show"
+            />
+            <div className="items-list-content-container">
+                {!getDataError ? (
+                    <>
+                        <div className="show-items-top-text">
+                            <span>
+                                Please read the{" "}
+                                <a href="/showguidelines">rules</a>. You can
+                                also browse the <a href="/shownew">newest</a>{" "}
+                                Show submissions.
+                            </span>
+                        </div>
+                        <ItemsList
+                            items={items}
+                            goToString={goToString}
+                            userSignedIn={authUserData.userSignedIn}
+                            currUsername={authUserData.username}
+                            showHideOption={true}
+                            showRank={true}
+                            isMoreLink={`/show?page=${page + 1}`}
+                            isMore={isMore}
+                        />
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const apiResult = await getRankedShowItemsByPage(page, req);
+    // console.log(apiResult);
+
+    return {
+        props: {
+            items: (apiResult && apiResult.items) || {},
+            authUserData:
+                apiResult && apiResult.authUser ? apiResult.authUser : {},
+            page: page || 0,
+            isMore: (apiResult && apiResult.isMore) || false,
+            getDataError: (apiResult && apiResult.getDataError) || false,
+            goToString: page > 1 ? `news?page=${page}` : "show",
+        },
+    };
+}
