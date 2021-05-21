5c1d51e98545d3dad0c8e66fda6b71ac0b4c4f82
krehwell <fahrimuham001@gmail.com>
2021-05-21 17:45:51 +0800

    WS-FE: add submitted page

---
+++ website/pages/submitted.js
@@ -0,0 +1,81 @@
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import ItemsList from "../components/itemsList.js";
+
+import getItemsSubmittedByUser from "../api/items/getItemsSubmittedByUser.js";
+
+export default function Submitted({
+    items,
+    authUserData,
+    page,
+    userId,
+    isMore,
+    getDataError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata
+                title={
+                    userId
+                        ? `${userId}'s Submissions | HeckarNews`
+                        : "HeckarNews"
+                }
+            />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+            />
+            <div className="items-list-content-container">
+                {!getDataError ? (
+                    <>
+                        <ItemsList
+                            items={items}
+                            goToString={goToString}
+                            userSignedIn={authUserData.userSignedIn}
+                            currUsername={authUserData.username}
+                            showRank={true}
+                            showPastLink={true}
+                            showWebLink={true}
+                            isMoreLink={`/submitted?id=${
+                                userId
+                            }&page=${page + 1}`}
+                            isMore={isMore}
+                        />
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        <span>An error occurred.</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const userId = query.id ? query.id : "";
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const apiResult = await getItemsSubmittedByUser(userId, page, req);
+
+    return {
+        props: {
+            items: (apiResult && apiResult.items) || {},
+            authUserData:
+                apiResult && apiResult.authUser ? apiResult.authUser : {},
+            page: page || 0,
+            userId: userId || "",
+            isMore: (apiResult && apiResult.isMore) || false,
+            getDataError: (apiResult && apiResult.getDataError) || false,
+            goToString:
+                page > 1
+                    ? `submitted?id=${userId}&page=${page}`
+                    : `submitted?id=${userId}`,
+        },
+    };
+}
