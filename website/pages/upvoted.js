71f5358377a99a5c0b087e8cd882f433fa80ca84
krehwell <fahrimuham001@gmail.com>
2021-05-22 14:40:25 +0800

    WS-FE: add user upvote page

---
+++ website/pages/upvoted.js
@@ -0,0 +1,117 @@
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import HeadMetadata from "../components/headMetadata.js";
+import ItemsList from "../components/itemsList.js";
+
+import getUserUpvotedItemsByPage from "../api/items/getUserUpvotedItemsByPage.js";
+
+export default function Upvoted({
+    items,
+    showItems,
+    isMoreItems,
+    comments,
+    showComments,
+    isMoreComments,
+    authUserData,
+    userId,
+    page,
+    getDataError,
+    notAllowedError,
+    goToString,
+}) {
+    return (
+        <div className="layout-wrapper">
+            <HeadMetadata
+                title={`Upvoted ${
+                    showItems ? "Items" : "Comments"
+                } | HeckarNews`}
+            />
+            <Header
+                userSignedIn={authUserData && authUserData.userSignedIn}
+                username={authUserData && authUserData.username}
+                karma={authUserData && authUserData.karma}
+                goto={goToString}
+                label="upvoted"
+            />
+            <div className="items-list-content-container">
+                {!getDataError && !notAllowedError ? (
+                    <>
+                        {showItems ? (
+                            <ItemsList
+                                items={items}
+                                goToString={goToString}
+                                userSignedIn={authUserData.userSignedIn}
+                                currUsername={authUserData.username}
+                                showRank={true}
+                                isMoreLink={`/upvoted?id=${userId}&page=${
+                                    page + 1
+                                }`}
+                                isMore={isMoreItems}
+                            />
+                        ) : null}
+                        {showComments ? <></> : null}
+                    </>
+                ) : (
+                    <div className="items-list-error-msg">
+                        {notAllowedError ? (
+                            <span>Can’t display that.</span>
+                        ) : (
+                            <span>An error occurred.</span>
+                        )}
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, query }) {
+    const userId = query.id ? query.id : "";
+    const page = query.page ? parseInt(query.page) : 1;
+
+    const showItems = query.comments === "t" ? false : true;
+
+    let itemsApiResult, commentsApiResult, authUserData;
+
+    if (showItems) {
+        itemsApiResult = await getUserUpvotedItemsByPage(userId, page, req);
+
+        authUserData = itemsApiResult.authUser ? itemsApiResult.authUser : {};
+
+        commentsApiResult = {};
+    } else {
+        // make api call to get comments data
+    }
+
+    const goToString =
+        page > 1
+            ? `upvoted?id=${userId}${
+                  showItems ? "" : "&comments=t"
+              }&page=${page}`
+            : `upvoted?id=${userId}${showItems ? "" : "&comments=t"}`;
+
+    return {
+        props: {
+            items: (itemsApiResult && itemsApiResult.items) || [],
+            showItems: showItems || false,
+            isMoreItems: (itemsApiResult && itemsApiResult.isMore) || false,
+            comments: (commentsApiResult && commentsApiResult.comments) || [],
+            showComments: !showItems,
+            isMoreComments:
+                (commentsApiResult && commentsApiResult.isMore) || false,
+            authUserData: authUserData || {},
+            userId: userId || "",
+            page: page || 0,
+            getDataError:
+                itemsApiResult.getDataError ||
+                commentsApiResult.getDataError ||
+                false,
+            notAllowedError:
+                itemsApiResult.notAllowedError ||
+                commentsApiResult.notAllowedError ||
+                false,
+            goToString: goToString || "",
+        },
+    };
+}
