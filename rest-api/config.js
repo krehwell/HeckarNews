bde4804a4343f7ca81db3f686500dc3205dcf9a5
krehwell <fahrimuham001@gmail.com>
2021-05-22 22:55:30 +0800

    WS-BE: config new item score

--- rest-api/config.js
+++ rest-api/config.js
@@ -1,10 +1,18 @@
 const config = {
     productionWebsiteUrl: "https://news.krehwell.com",
+
+    /// SITE CONFIGURATION
     userCookieExpirationLengthInDays: 365,
+
+    /// ITEM CONFIGURATION
     hrsUntilUnvoteExpires: 24 * 30,
     hrsUntilEditAndDeleteExpires: 24 * 30,
     maxAgeOfRankedItemsInDays: 360,
     itemsPerPage: 30,
+
+    /// ITEM SCORE ALGORITHM CONFIGURATION
+    scoreGravity: 1.8,
+    updateScoreTimeScheduleInMinute: 10
 };
 
 module.exports = config;
