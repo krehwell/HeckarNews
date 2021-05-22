1d9d5bfb5473b1ce7e4090e5c67129ba696e3802
krehwell <fahrimuham001@gmail.com>
2021-05-23 00:23:15 +0800

    WS-FE: loading site with nprogress instead

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,11 +1,18 @@
-/// STYLES
+import NProgress from "nprogress";
+import Router from "next/router";
+
+/// GLOBAL STYLES
+import "../styles/nprogress.css";
 import "../styles/layout.css";
+
+/// COMPONENT STYLES
 import "../styles/components/header.css";
 import "../styles/components/footer.css";
 import "../styles/components/alternateHeader.css";
 import "../styles/components/item.css";
 import "../styles/components/itemsList.css";
 
+/// PAGES STYLES
 import "../styles/pages/_error.css";
 import "../styles/pages/login.css";
 import "../styles/pages/forgot.css";
@@ -19,6 +26,12 @@ import "../styles/pages/delete-item.css";
 import "../styles/pages/show.css";
 import "../styles/pages/past.css";
 import "../styles/pages/favorites.css";
+
+/// NProgress CONFIG
+NProgress.configure({ easing: "ease", speed: 500, showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
