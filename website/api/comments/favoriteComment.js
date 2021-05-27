1b39be90ebd45abd309cfd26f8ca7f29ce2ed684
krehwell <fahrimuham001@gmail.com>
2021-05-28 04:05:23 +0800

    WS-FE: add favorite comment api

---
+++ website/api/comments/favoriteComment.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+import apiBaseUrl from "../../utils/apiBaseUrl.js";
+
+export default function favoriteComment(commentId, callback) {
+    axios
+        .post(
+            apiBaseUrl + "/comments/favorite-comment",
+            {
+                id: commentId,
+            },
+            {
+                withCredentials: true,
+            }
+        )
+        .then(function (response) {
+            callback(response.data);
+        })
+        .catch(function (error) {
+            console.log("ERR", error);
+            callback({ submitError: true });
+        });
+}
