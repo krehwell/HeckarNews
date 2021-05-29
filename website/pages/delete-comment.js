0c2aee5712691831fa531447c8c6ba3637a30617
krehwell <fahrimuham001@gmail.com>
2021-05-29 16:40:11 +0800

    WS-FE: add submit delete comment to page

--- website/pages/delete-comment.js
+++ website/pages/delete-comment.js
@@ -8,6 +8,7 @@ import renderCreatedTime from "../utils/renderCreatedTime.js";
 import truncateItemTitle from "../utils/truncateItemTitle.js";
 
 import getDeleteCommentPageData from "../api/comments/getDeleteCommentPageData.js";
+import deleteComment from "../api/comments/deleteComment.js";
 
 export default function DeleteComment({
     comment,
@@ -20,10 +21,31 @@ export default function DeleteComment({
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState({
         submitError: false,
+        notAllowedError: notAllowedError,
     });
 
     const submitDeleteComment = () => {
         if (loading) return;
+
+        setLoading(true);
+
+        deleteComment(comment.id, (response) => {
+            if (response.notAllowedError) {
+                setError({
+                    notAllowedError: true,
+                    submitError: false,
+                });
+            } else if (response.submitError || !response.success) {
+                setError({
+                    notAllowedError: false,
+                    submitError: true,
+                });
+            } else {
+                window.location.href = `/${goToString}`;
+            }
+
+            setLoading(false);
+        });
     };
 
     const goBackToOriginPage = () => {
@@ -37,30 +59,38 @@ export default function DeleteComment({
             <HeadMetadata title="Delete Comment | HeckarNews" />
             <AlternateHeader displayMessage="Delete Comment" />
             <div className="delete-comment-content-container">
-                {!getDataError && !error.notAllowedError && !error.notFoundError ? (
+                {!getDataError && !error.notAllowedError && !notFoundError ? (
                     <>
                         <div className="delete-comment-top-section">
                             <table>
                                 <tbody>
                                     <tr>
                                         <td valign="top">
+                                            {/* VOTE COMMENT, SHOULD BE STAR SINCE IT MUST BE USERS COMMENT */}
                                             <div className="delete-comment-top-section-star">
                                                 <span>*</span>
                                             </div>
                                         </td>
                                         <td>
+                                            {/* COMMENT POINT */}
                                             <span className="delete-comment-top-section-points">
-                                                {comment.points.toLocaleString()} {renderPointsString(comment.points)}
+                                                {comment?.points?.toLocaleString()} {renderPointsString(comment.points)}
                                             </span>
+
+                                            {/* COMMENT BY */}
                                             <span>
                                                 by <a href={`/user?id=${comment.by}`}>{comment.by}</a>&nbsp;
                                             </span>
+
+                                            {/* COMMENT CREATED TIME */}
                                             <span>
                                                 <a href={`/comment?id=${comment.id}`}>
                                                     {renderCreatedTime(comment.created)}
                                                 </a>
                                             </span>
                                             <span> | </span>
+
+                                            {/* PARENT */}
                                             <span className="delete-comment-top-section-parent">
                                                 <a
                                                     href={
@@ -72,16 +102,22 @@ export default function DeleteComment({
                                                 </a>
                                             </span>
                                             <span> | </span>
+
+                                            {/* EDIT COMMENT */}
                                             <span>
                                                 <a href={`/edit-comment?id=${comment.id}`}>edit</a>
                                             </span>
                                             <span> | </span>
+
+                                            {/* COMMENT FROM POST/ITEM */}
                                             <span className="delete-comment-top-section-article-title">
                                                 on:&nbsp;
                                                 <a href={`/item?id=${comment.parentItemId}`}>
-                                                    {truncateItemTitle(comment.parentItemTitle)}
+                                                    {truncateItemTitle(comment?.parentItemTitle)}
                                                 </a>
                                             </span>
+
+                                            {/* COMMENT CONTENT */}
                                             <div className="delete-comment-content-text">
                                                 <span dangerouslySetInnerHTML={{ __html: comment.text }}></span>
                                             </div>
@@ -90,6 +126,8 @@ export default function DeleteComment({
                                 </tbody>
                             </table>
                         </div>
+
+                        {/* DELETE COMMENT BUTTON */}
                         <div className="delete-comment-confirm-msg">
                             <span>Do you want to delete this comment?</span>
                         </div>
@@ -101,6 +139,7 @@ export default function DeleteComment({
                                 onClick={submitDeleteComment}
                             />
                             <input type="submit" value="No" onClick={goBackToOriginPage} />
+                            {loading && <span> loading...</span>}
                         </div>
                         {error.submitError ? (
                             <div className="delete-comment-submit-error-msg">
@@ -111,8 +150,8 @@ export default function DeleteComment({
                 ) : (
                     <div className="delete-comment-error-msg">
                         {getDataError ? <span>An error occurred.</span> : null}
+                        {notFoundError ? <span>Comment not found.</span> : null}
                         {error.notAllowedError ? <span>You can’t delete that comment.</span> : null}
-                        {error.notFoundError ? <span>Comment not found.</span> : null}
                     </div>
                 )}
             </div>
