a20bb3f7064483c238b46978969e08fb40bf3021
krehwell <fahrimuham001@gmail.com>
2021-06-01 02:13:34 +0800

    WS-FE: modify get comment by id api to send page num of comment

--- website/api/comments/getCommentById.js
+++ website/api/comments/getCommentById.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 import apiBaseUrl from "../../utils/apiBaseUrl.js";
 
-export default async function getCommentById(commentId, req) {
+export default async function getCommentById(commentId, page, req) {
     try {
         const cookie = req.headers.cookie ? req.headers.cookie : "";
 
         const response = await axios({
-            url: `${apiBaseUrl}/comments/get-comment-by-id?id=${commentId}`,
+            url: `${apiBaseUrl}/comments/get-comment-by-id?id=${commentId}&page={page}`,
             headers: req ? { cookie: cookie } : "",
             withCredentials: true,
         });
