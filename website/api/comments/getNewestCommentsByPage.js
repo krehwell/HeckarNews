b939205f8e92e1ea7c85030f3e5373b221ef0b8e
krehwell <fahrimuham001@gmail.com>
2021-06-02 17:10:59 +0800

    WS-FE: get newest comment api

---
+++ website/api/comments/getNewestCommentsByPage.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+import apiBaseUrl from "../../utils/apiBaseUrl.js";
+
+export default async function getNewestCommentsByPage(page, req) {
+    try {
+        const cookie = req.headers.cookie ? req.headers.cookie : "";
+
+        const response = await axios({
+            url: `${apiBaseUrl}/comments/get-newest-comments-by-page?page=${page}`,
+            headers: req ? { cookie: cookie } : "",
+            withCredentials: true,
+        });
+
+        return response.data;
+    } catch (error) {
+        return { getDataError: true };
+    }
+}
