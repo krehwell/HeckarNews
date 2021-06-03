7b8bc5b121490c3e85681e27dd549e9c3dbae9db
krehwell <fahrimuham001@gmail.com>
2021-06-03 21:14:34 +0800

    WS-FE: user upvoted comment api

---
+++ website/api/comments/getUserUpvotedCommentsByPage.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+import apiBaseUrl from "../../utils/apiBaseUrl.js";
+
+export default async function getUserUpvotedCommentsByPage(userId, page, req) {
+    try {
+        const cookie = req.headers.cookie ? req.headers.cookie : "";
+
+        const response = await axios({
+            url: `${apiBaseUrl}/comments/get-user-upvoted-comments-by-page?userId=${userId}&page=${page}`,
+            headers: req ? { cookie: cookie } : "",
+            withCredentials: true,
+        });
+
+        return response.data;
+    } catch (error) {
+        return { getDataError: true };
+    }
+}
