5c828e3b1eff28fc5b0f3265d1a30cfb0920f79c
krehwell <fahrimuham001@gmail.com>
2021-06-03 20:07:28 +0800

    WS-FE: get ccomment by user api

---
+++ website/api/comments/getUserCommentsByPage.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import apiBaseUrl from "../../utils/apiBaseUrl.js";
+
+export default async function getUserCommentsByPage(userId, page, req) {
+    try {
+        const cookie = req.headers.cookie ? req.headers.cookie : "";
+
+        console.log("FUCK YOU");
+
+        const response = await axios({
+            url: `${apiBaseUrl}/comments/get-user-comments-by-page?userId=${userId}&page=${page}`,
+            headers: req ? { cookie: cookie } : "",
+            withCredentials: true,
+        });
+
+        return response.data;
+    } catch (error) {
+        return { getDataError: true };
+    }
+}
