6040684b14cdd2cd3d3e328ae5058b630c8fbc4a
krehwell <fahrimuham001@gmail.com>
2021-06-07 07:37:04 +0800

    WS-FE: wrapper .env to next.config on client

--- website/next.config.js
+++ website/next.config.js
@@ -2,5 +2,7 @@ module.exports = {
     env: {
         DEVELOPMENT_API_URL: "http://localhost:5000",
         PRODUCTION_API_URL: "https://api.heckarnews.com",
+        ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
+        ALGOLIA_PUBLIC_API_KEY: process.env.ALGOLIA_PUBLIC_API_KEY,
     },
 };
