3474e422b04ddab42ff1dc436804c764c72dee98
krehwell <fahrimuham001@gmail.com>
2021-06-07 05:10:05 +0800

    WS-FE: search components footer & header

---
+++ website/components/search/footer.js
@@ -0,0 +1,19 @@
+export default function SearchFooter({}) {
+    return (
+        <div className="search-footer">
+            <ul>
+                <li>
+                    <a href="/search/about">About</a>
+                </li>
+                <li>•</li>
+                <li>
+                    <a href="/search/settings">Settings</a>
+                </li>
+                <li>•</li>
+                <li>
+                    <a href="/">Coder News</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
---
+++ website/components/search/header.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+import SearchBarIcon from "./svg/searchBarIcon.js";
+import AlgoliaLogo from "./svg/algoliaLogo.js";
+import SettingsIcon from "./svg/settingsIcon.js";
+import LeftArrow from "./svg/leftArrow.js";
+
+export default function SearchPageHeader({ showSearchBar, searchQuery, showSettingsButton, showBackButton }) {
+    const [searchInputValue, setSearchInputValue] = useState(searchQuery ? searchQuery : "");
+
+    const updateSearchInputValue = (event) => {
+        setSearchInputValue(event.target.value);
+    };
+
+    const checkForEnterKeypress = (event) => {
+        if (event.keyCode === 13 || event.which === 13) {
+            submitSearchInputRequest(event.target.value);
+        }
+    };
+
+    const submitSearchInputRequest = (inputValue) => {
+        const query = `q=${inputValue}`;
+
+        window.location.href = `/search?${query}`;
+    };
+
+    return (
+        <div className="search-header">
+            <a className="search-header-logo" href="/search">
+                <img src="/android-chrome-512x512.png" />
+                <div className="search-header-logo-label">
+                    <span>
+                        Search <br />
+                        HeckarNews
+                    </span>
+                </div>
+            </a>
+
+            {/* SEARCH BAR */}
+            {showSearchBar ? (
+                <div className="search-header-bar">
+                    <span className="search-header-bar-icon">
+                        <SearchBarIcon />
+                    </span>
+                    <input
+                        type="search"
+                        placeholder="Search stories by title, url, or author"
+                        value={searchInputValue}
+                        onChange={updateSearchInputValue}
+                        onKeyUp={checkForEnterKeypress}
+                    />
+                    <div className="search-header-bar-powered-by">
+                        <span className="search-header-bar-powered-by-label">Search by</span>
+                        <a href="https://www.algolia.com">
+                            <AlgoliaLogo />
+                        </a>
+                    </div>
+                </div>
+            ) : null}
+
+            {/* SETTINGS BTN */}
+            {showSettingsButton ? (
+                <div className="search-header-settings">
+                    <a href="/search/settings">
+                        <SettingsIcon />
+                        <span className="search-header-settings-label">Settings</span>
+                    </a>
+                </div>
+            ) : null}
+
+            {/* BACK BTN */}
+            {
+                showBackButton ?
+                    <div className="search-header-back">
+                        <a href="/search">
+                            <LeftArrow />
+                            Back
+                        </a>
+                    </div> : null
+            }
+
+        </div>
+    );
+}
