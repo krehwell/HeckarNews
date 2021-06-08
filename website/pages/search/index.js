26d6f6dda12f39c430ecaef2b531b407623ab2be
krehwell <fahrimuham001@gmail.com>
2021-06-09 01:41:03 +0800

    WS-FE: index receive props for additional query

--- website/pages/search/index.js
+++ website/pages/search/index.js
@@ -6,27 +6,50 @@ import Comment from "../../components/search/comment.js";
 
 import getAlgoliaData from "../../api/search/getAlgoliaData.js";
 
-export default function Search({ searchQuery, hits, getDataError }) {
+export default function Search({
+    searchQuery,
+    hits,
+    getDataError,
+    totalNumOfHits,
+    processingTimeMS,
+    itemType,
+    sortBy,
+    dateRange,
+    startDate,
+    endDate,
+    currPageNumber,
+    totalNumOfPages,
+}) {
     return (
         <div className="search-wrapper">
             <HeadMetadata title="Search | HeckarNews" />
-            <SearchPageHeader searchQuery={searchQuery} showSearchBar showSettingsButton showBackButton />
+            <SearchPageHeader
+                searchQuery={searchQuery}
+                showSearchBar={true}
+                showSettingsButton={true}
+                currPageNumber={currPageNumber}
+                itemType={itemType}
+                dateRange={dateRange}
+                startDate={startDate}
+                endDate={endDate}
+                sortBy={sortBy}
+            />
             <div className="search-results">
                 <div className="search-results-items">
                     {hits.length > 0 && !getDataError
-                        ? hits.map((hit, index) => {
-                              return hit.type === "item" ? (
-                                  <>
-                                      <div
-                                          style={{
-                                              padding: "8px 0px",
-                                              borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
-                                          }}>
+                        ? hits.map((hit, _) => {
+                              return (
+                                  <div
+                                      style={{
+                                          padding: "8px 0px",
+                                          borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+                                      }}>
+                                      {hit.type === "item" ? (
                                           <Item item={hit} key={hit.objectID} />
-                                      </div>
-                                  </>
-                              ) : (
-                                  <Comment comment={hit} key={hit.objectID} />
+                                      ) : (
+                                          <Comment comment={hit} key={hit.objectID} />
+                                      )}
+                                  </div>
                               );
                           })
                         : null}
@@ -50,6 +73,15 @@ export async function getServerSideProps({ query }) {
             searchQuery: query.q ? query.q : "",
             hits: apiResult.hits ? apiResult.hits : [],
             getDataError: apiResult.error || false,
+            totalNumOfHits: apiResult.nbHits,
+            processingTimeMS: apiResult.processingTimeMS || 0,
+            itemType: apiResult.itemType ? apiResult.itemType : "",
+            sortBy: apiResult.sortBy ? apiResult.sortBy : "",
+            dateRange: apiResult.dateRange ? apiResult.dateRange : "",
+            startDate: query.startDate ? query.startDate : "",
+            endDate: query.endDate ? query.endDate : "",
+            currPageNumber: apiResult.page || 1,
+            totalNumOfPages: apiResult.nbPages || 1,
         },
     };
 }
