20d89c435e2411a860dc7366c5feec42f985f0d3
krehwell <fahrimuham001@gmail.com>
2021-06-10 06:46:24 +0800

    WS-FE: more icons for date picker

---
+++ website/components/search/svg/cancelIcon.js
@@ -0,0 +1,18 @@
+export default function CancelIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="15" y1="9" x2="9" y2="15"></line>
+            <line x1="9" y1="9" x2="15" y2="15"></line>
+        </svg>
+    );
+}
