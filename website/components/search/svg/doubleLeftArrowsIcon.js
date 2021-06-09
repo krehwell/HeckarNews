bae54fe44ad2c3f978c0d39bed5f5b36ef871441
krehwell <fahrimuham001@gmail.com>
2021-06-10 07:46:09 +0800

    WS-FE: double icons

---
+++ website/components/search/svg/doubleLeftArrowsIcon.js
@@ -0,0 +1,17 @@
+export default function DoubleLeftArrowsIcon({}) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <polyline points="11 17 6 12 11 7"></polyline>
+            <polyline points="18 17 13 12 18 7"></polyline>
+        </svg>
+    );
+}
---
+++ website/components/search/svg/doubleRightArrowsIcon.js
@@ -0,0 +1,17 @@
+export default function DoubleRightArrowIcons({}) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <polyline points="13 17 18 12 13 7"></polyline>
+            <polyline points="6 17 11 12 6 7"></polyline>
+        </svg>
+    );
+}
