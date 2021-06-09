61bbd760e77187b488c2e942db03fdb275a0d36b
krehwell <fahrimuham001@gmail.com>
2021-06-10 05:24:29 +0800

    WS-FE: add up and down arrow svg

---
+++ website/components/search/svg/upArrow.js
@@ -0,0 +1,16 @@
+export default function UpArrow({}) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <polyline points="18 15 12 9 6 15"></polyline>
+        </svg>
+    );
+}
