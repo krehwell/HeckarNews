08eba065b5057c6ec281ac0d2b30007dfbd6feb6
krehwell <fahrimuham001@gmail.com>
2021-06-12 00:19:36 +0800

    WS-BE: kill item endpoint

--- rest-api/routes/moderation/api.js
+++ rest-api/routes/moderation/api.js
@@ -1,4 +1,35 @@
+const moment = require("moment");
+
 const ModerationLogModel = require("../../models/moderationLog.js");
+const ItemModel = require("../../models/item");
+const searchApi = require("../../routes/search/api.js");
 
 module.exports = {
+    killItem: async (itemId, moderator) => {
+        const item = await ItemModel.findOneAndUpdate(
+            { id: itemId },
+            { $set: { dead: true, score: 0 } }
+        )
+            .lean()
+            .exec();
+
+        if (!item) {
+            throw { submitError: true };
+        }
+
+        await searchApi.deleteItem(itemId);
+
+        const newModerationLogDoc = new ModerationLogModel({
+            moderatorUsername: moderator.username,
+            actionType: "kill-item",
+            itemId: itemId,
+            itemTitle: item.title,
+            itemBy: item.by,
+            created: moment().unix(),
+        });
+
+        await newModerationLogDoc.save();
+
+        return { success: true };
+    },
 };
--- rest-api/routes/moderation/index.js
+++ rest-api/routes/moderation/index.js
@@ -1,11 +1,26 @@
-const express = require("express")
+const express = require("express");
 
-const api = require("./api.js")
+const api = require("./api.js");
 
-const authUser = require("../../middlewares/index.js").authUser
+const authUser = require("../../middlewares/index.js").authUser;
 
-const app = express.Router()
+const app = express.Router();
 
 /// MODERATION API ENDPOINT
+app.put("/moderation/kill-item", authUser, async (req, res) => {
+    try {
+        if (!res.locals.userSignedIn || !res.locals.isModerator) {
+            res.json({ authError: true });
+        }
+        const response = await api.killItem(req.body.id, res.locals);
+        res.json(response);
+    } catch (error) {
+        if (!(error instanceof Error)) {
+            res.json(error);
+        } else {
+            res.json({ submitError: true });
+        }
+    }
+});
 
-module.exports = app
+module.exports = app;
