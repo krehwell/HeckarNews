fc87235f205377fe87f3ce3ff6de692d34c7d87c
krehwell <fahrimuham001@gmail.com>
2021-06-21 16:18:06 +0800

    WS-BE: fix any url mistaken

--- rest-api/routes/users/index.js
+++ rest-api/routes/users/index.js
@@ -121,7 +121,7 @@ app.put("/users/logout", authUser, async (req, res) => {
             domain:
                 process.env.NODE_ENV === "development"
                     ? ""
-                    : utils.getDomainFromUrl(config.productionWebsiteURL),
+                    : utils.getDomainFromUrl(config.productionWebsiteUrl),
         };
 
         res.clearCookie("user", cookieSettings);
@@ -148,7 +148,7 @@ app.put("/users/remove-user-cookie-data", (req, res) => {
         domain:
             process.env.NODE_ENV === "development"
                 ? ""
-                : utils.getDomainFromUrl(config.productionWebsiteURL),
+                : utils.getDomainFromUrl(config.productionWebsiteUrl),
     };
 
     res.cookieSettings("user", cookieSettings);
@@ -278,7 +278,7 @@ app.put("/users/change-password", authUser, async (req, res) => {
                 domain:
                     process.env.NODE_ENV === "development"
                         ? ""
-                        : utils.getDomainFromUrl(config.productionWebsiteURL),
+                        : utils.getDomainFromUrl(config.productionWebsiteUrl),
             };
 
             res.clearCookie("user", cookieSettings);
