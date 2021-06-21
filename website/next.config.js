f84efde271d30624a85153ca52ee8d9fb70dc48a
krehwell <fahrimuham001@gmail.com>
2021-06-21 15:36:36 +0800

    WS-FE: fix rest api url

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         DEVELOPMENT_API_URL: "http://localhost:5000",
-        PRODUCTION_API_URL: "https://forum-api.krehwell.comr,
+        PRODUCTION_API_URL: "https://forum-api.krehwell.com",
         ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
         ALGOLIA_PUBLIC_API_KEY: process.env.ALGOLIA_PUBLIC_API_KEY,
     },
